d0acb7d9577611f0bbbeed6d6603e741d0694ad8
Antony <miroliubov.a@yandex.ru>
2018-08-28 20:15:12 +0300

    wip
    Добавил проверку на ввод числа.
    Добавил проверку строки на регистр.
    Провел небольшой рефакторинг.

--- array_learning/transliterationFunction.php
+++ array_learning/transliterationFunction.php
@@ -1,26 +1,64 @@
 <?php
-
-$someString = 'абв';
+$someString = '';
 function translate($someString){
-$letterArray = [
-    'а' => 'a',
-    'б' => 'b',
-    'в' => 'v',
-    'г' => 'g',
-    'д' => 'd',
-    'е' => 'e'
-];
+    $letterArray = [
+        'а' => 'a',
+        'б' => 'b',
+        'в' => 'v',
+        'г' => 'g',
+        'д' => 'd',
+        'е' => 'e',
+        'ё' => 'yo',
+        'ж' => 'j',
+        'з' => 'z',
+        'и' => 'i',
+        'й' => '',
+        'к' => 'k',
+        'л' => 'l',
+        'м' => 'm',
+        'н' => 'n',
+        'о' => 'o',
+        'п' => 'p',
+        'р' => 'r',
+        'с' => 's',
+        'т' => 't',
+        'у' => 'u',
+        'ф' => 'f',
+        'х' => 'h',
+        'ц' => '',
+        'ч' => 'ch',
+        'ш' => 'sh',
+        'щ' => '',
+        'Ъ' => '',
+        'ы' => '',
+        'ь' => '',
+        'э' => '',
+        'ю' => 'y',
+        'я' => 'ja',
 
-$arr = str_split($someString, 2);
-$emptyArr = [];
-foreach($letterArray as $k => $v) {
-    foreach($arr as $value) {
-        if ($k == $value) {
-            $emptyArr = $v;
-            echo $emptyArr;
+        'a' => 'а',
+        'b' => 'б'
+    ];
+    $someString = mb_strtolower($someString);
+    $stringAsArray = str_split($someString,2);
+    $result = [];
+    foreach($letterArray as $k => $v) {
+
+        foreach ($stringAsArray as $value) {
+
+            if ($k == $value) :
+                array_push($result, $v);
+            print_r($result);
+
+            elseif (is_int($someString)) :
+                echo 'Переданное значение - число';
+
+            endif;
         }
+
     }
-}
+    return implode($result);
 };
-echo translate('аббв');
+echo translate('АНТОН');
+
 ?>

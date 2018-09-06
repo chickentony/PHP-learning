ff7e4cde1ff887e665f950472c37e65088c13141
Antony <miroliubov.a@yandex.ru>
2018-09-06 19:12:32 +0300

    WIP

--- index.php
+++ index.php
@@ -1,53 +1,58 @@
 <?php
 $someString = '';
-function translate($someString){
-    $letterArray = [
-        'а' => 'a',
-        'б' => 'b',
-        'в' => 'v',
-        'г' => 'g',
-        'д' => 'd',
-        'е' => 'e',
-        'ё' => 'yo',
-        'ж' => 'j',
-        'з' => 'z',
-        'и' => 'i',
-        'й' => '',
-        'к' => 'k',
-        'л' => 'l',
-        'м' => 'm',
-        'н' => 'n',
-        'о' => 'o',
-        'п' => 'p',
-        'р' => 'r',
-        'с' => 's',
-        'т' => 't',
-        'у' => 'u',
-        'ф' => 'f',
-        'х' => 'h',
-        'ц' => '',
-        'ч' => 'ch',
-        'ш' => 'sh',
-        'щ' => '',
-        'Ъ' => '',
-        'ы' => '',
-        'ь' => '',
-        'э' => '',
-        'ю' => 'y',
-        'я' => 'ja',
+$a = '';
+$separator = '_';
+$letterArray = [
+    'а' => 'a',
+    'б' => 'b',
+    'в' => 'v',
+    'г' => 'g',
+    'д' => 'd',
+    'е' => 'e',
+    'ё' => 'yo',
+    'ж' => 'j',
+    'з' => 'z',
+    'и' => 'i',
+    'й' => '',
+    'к' => 'k',
+    'л' => 'l',
+    'м' => 'm',
+    'н' => 'n',
+    'о' => 'o',
+    'п' => 'p',
+    'р' => 'r',
+    'с' => 's',
+    'т' => 't',
+    'у' => 'u',
+    'ф' => 'f',
+    'х' => 'h',
+    'ц' => '',
+    'ч' => 'ch',
+    'ш' => 'sh',
+    'щ' => '',
+    'Ъ' => '',
+    'ы' => '',
+    'ь' => '',
+    'э' => '',
+    'ю' => 'y',
+    'я' => 'ja',
+    '_' => '_',
 
-        'a' => 'а',
-        'b' => 'б',
-        'c' => 'с',
-        'd' => 'д',
-        'e' => 'е',
-        'f' => 'ф',
-        'g' => 'г',
-        'h' => 'х',
-        'i' => '',
-        'j' => 'ж',
-        'k' => 'к'
-    ];
+    'a' => 'а',
+    'b' => 'б',
+    'c' => 'с',
+    'd' => 'д',
+    'e' => 'е',
+    'f' => 'ф',
+    'g' => 'г',
+    'h' => 'х',
+    'i' => '',
+    'j' => 'ж',
+    'k' => 'к'
+];
+
+function translate($someString){
+
     if(!is_string($someString)) {
         echo 'Полученные данные, не являются строкой';
     }
@@ -58,37 +63,44 @@ function translate($someString){
 //    elseif (preg_match('/[[:punct:]]|\s/', $someString)){
 //        echo 'В строке есть спецсимволы';
 //    }
+      $resultArray  = removeSpaces($someString);
+//    var_dump($resultArray);
 
-    $someString = mb_strtolower($someString);
-    $stringAsArray = str_split($someString,2);
+    $resultArray  = mb_strtolower($resultArray);
+//    var_dump($resultArray );
+    $stringAsArray = preg_split('//u',$resultArray,-1,PREG_SPLIT_NO_EMPTY);
+//    var_dump($stringAsArray);
     $result = [];
+    global $letterArray;
 
         foreach ($stringAsArray as $value) {
+//            var_dump($value);
 
             foreach($letterArray as $k => $v) {
 
-            if ($k == $value) :
+            if ($k === $value) :
                 array_push($result, $v);
+//                var_dump($result);
             endif;
         }
 
     }
+//    var_dump($result);
     return implode($result);
+
 };
-echo translate('лол[]');
+echo translate('Лол невероятно');
 echo "<br>";
 
-$a = '';
-$separator = '_';
 
-function removeSpaces($a) {
+function removeSpaces($someString) {
     global $separator;
 
-    if(!is_string($a)) {
+    if(!is_string($someString)) {
         echo 'В функцию передана не строка';
     }
-    elseif (is_string($a)) {
-        $res = preg_split('//u',$a,-1,PREG_SPLIT_NO_EMPTY);
+    elseif (is_string($someString)) {
+        $res = preg_split('//u',$someString,-1,PREG_SPLIT_NO_EMPTY);
 
         for($index = 0; $index < count($res); $index++) {
             if ($res[$index] === ' ') {
@@ -100,6 +112,4 @@ function removeSpaces($a) {
     }
     return implode($res);
 };
-echo removeSpaces('это просто невероятно lox htr');
-
-?>
+//echo removeSpaces('это просто невероятно lox htr');

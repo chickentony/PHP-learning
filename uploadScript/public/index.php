06449f22e4c12cd1e27eef56c5fb1c36505d3d2a
Antony <miroliubov.a@yandex.ru>
2018-09-15 18:09:23 +0300

    fixes and refactor

--- uploadScript/public/index.php
+++ uploadScript/public/index.php
@@ -7,8 +7,7 @@ $uploadDir = 'upload/';
 $uploadFile = $uploadDir . ($_FILES['userfile']['name']);
 $error = $_FILES['userfile']['error'];
 $expansion = ($_FILES['userfile']['type'] === 'image/jpg' || $_FILES['userfile']['type'] === 'image/jpeg' || $_FILES['userfile']['type'] === 'image/png' || $_FILES['userfile']['type'] === 'image/gif');
-//var_dump($error);
-//var_dump($_FILES['userfile']['type']);
+
 function generateExeption($error) {
     switch ($error) {
         case UPLOAD_ERR_INI_SIZE :
@@ -25,6 +24,7 @@ function generateExeption($error) {
             break;
     }
 }
+
 if ($_FILES['userfile']['error'] === UPLOAD_ERR_OK) {
     if ($expansion === true) {
         move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadFile);
@@ -38,25 +38,11 @@ else {
 }
 
 if (is_dir($uploadDir)){
-    
     $file = scandir($uploadDir);
     for($i = 0; $i < count($file); $i++) {
         if ($file[$i] !== '.' && $file[$i] !== '..') {
-        
-        $path = $uploadDir . $file[$i];   
-        echo "
-              <div class = 'container'>
-                <div class = 'row'>
-                    <div class = 'col'>
-                        <div class = 'float-left'>
-                            <img src = $path width = 300; height = 300; class = 'img-thumbnail'>
-                        </div>
-                    </div>
-                </div>
-              </div>";
-//            img_resize('../public/upload/', '../public/upload/', '300', '300', $rgb = 0xFFFFFF, $quality = 100);
+            $path = $uploadDir . $file[$i];   
+            echo "<a href='$path' target=_blank><img src = $path width = 300; height = 300; class = 'img-thumbnail'; style= 'margin-left: 20px';></a>";
         }
-//        closedir($uploadDir);
     }
 }
-

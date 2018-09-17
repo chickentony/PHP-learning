6c6e97613840a1381fa49908aed72681891be461
Antony <miroliubov.a@yandex.ru>
2018-09-17 21:22:46 +0300

    WIP1
    Убрал селект, добавил обработку арифметических действий по кнопкам.

--- calculator/action.php
+++ calculator/action.php
@@ -1,31 +1,30 @@
 <?php
-//var_dump($_POST); 
+var_dump($_POST); 
 //echo (int)$_POST['firstNumber'];
  
 if (isset($_POST['firstNumber']) && isset($_POST['secondNumber']) && $_POST['firstNumber'] !== '' && $_POST['secondNumber'] !== '') {
     $firstNumber = (int)$_POST['firstNumber'];
     $secondNumber = (int)$_POST['secondNumber'];
-    $mathOperation = $_POST['operations'];
+//    $mathOperation = $_POST['operations'];
 }
 else {
     echo 'Значение введено некорректно.';
     exit;
 }
 
-
-if ($mathOperation === 'dev' && $secondNumber === 0) {
+if (isset($_POST['dev']) && $_POST['dev'] === '/' && $secondNumber === 0) {
     echo 'На ноль делить нельзя.';
 }
-else if ($mathOperation === 'sub') {
+elseif (isset($_POST['sub']) && $_POST['sub'] === '-') {
     echo $firstNumber - $secondNumber;
 }
-else if ($mathOperation === 'sum') {
+elseif (isset($_POST['sum']) && $_POST['sum'] === '+') {
     echo $firstNumber + $secondNumber;
 }
-else if ($mathOperation === 'multiple') {
+elseif (isset($_POST['multiple']) && $_POST['multiple'] === '*') {
     echo $firstNumber * $secondNumber;
 }
-else if ($mathOperation === 'dev') {
+elseif (isset($_POST['dev']) && $_POST['dev'] === '/') {
     echo $firstNumber / $secondNumber;
 }
 

3a39b61a26438a3cf30fbf3c9aa5de7eda272d12
Antony <miroliubov.a@yandex.ru>
2018-09-19 20:43:42 +0300

    wip1

---
+++ array_learning/getSumNumber.php
@@ -0,0 +1,16 @@
+<?php
+
+function getSumNumber($number) {
+    $arr = [];
+    $result = 0;
+    $arr = str_split($number);
+    
+    //Решение с помощью встроенной функции.
+   //return array_sum($arr);
+   
+    for($i = 0; $i < count($arr); $i++) {
+        $result += $arr[$i]; 
+    }
+    return $result;
+}
+echo getSumNumber(123);

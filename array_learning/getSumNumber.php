28fca071ad8232d79c9e2d900cb90ebf4a0ad35f
Antony <miroliubov.a@yandex.ru>
2018-09-20 13:30:03 +0300

    added chek
    Добавил проверку на пустую строку.

--- array_learning/getSumNumber.php
+++ array_learning/getSumNumber.php
@@ -3,14 +3,21 @@
 function getSumNumber($number) {
     $arr = [];
     $result = 0;
-    $arr = str_split($number);
-    
-    //Решение с помощью встроенной функции.
-   //return array_sum($arr);
-   
-    for($i = 0; $i < count($arr); $i++) {
-        $result += $arr[$i]; 
+    if ($number === '') {
+        echo 'Данные некорректны';
     }
-    return $result;
+    else {
+        $arr = str_split($number);
+
+        //Решение с помощью встроенной функции.
+        //return array_sum($arr);
+
+        for ($i = 0; $i < count($arr); $i++) {
+            $result += $arr[$i];
+        }
+        return $result;
+    }
+
 }
+
 echo getSumNumber(123);

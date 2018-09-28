924bfc478457f8fe30b5c231509ae0f57ebfe91a
Antony <miroliubov.a@yandex.ru>
2018-09-28 18:07:23 +0300

    wip5
    Добавил функцию для работы с минутами.

--- array_learning/houresCount.php
+++ array_learning/houresCount.php
@@ -3,48 +3,63 @@
 function houresCount($degrees) {
 //    30 = 1 houre
 //    15 = 30 min;
-//    7.5 = 15 min
-//    3.75 = 7.5 min
-//    0.5 = 1 min
+
     $count = null;
     $res = null;
     $result = null;
     $min = null;
     $houers = null;
     $houerStr = '';
-    if ($degrees === 0) {
-        return $result = '12 : 00 пока не прошло и минуты.';
-    }
-    else if ($degrees === 360) {
-        return $result = 'Прошло 12 часов.';
-    }
-    else if ($degrees > 0 && $degrees <= 360) {
+    $minStr = '';
+//    if ($degrees === 0) {
+//        return $result = '12 : 00 пока не прошло и минуты.';
+//    }
+//    else if ($degrees === 360) {
+//        return $result = 'Прошло 12 часов.';
+//    }
+//    $degrees > 0 && $degrees <= 360
 
         //Получаем количество пройденных часов.
-        $res = $degrees / 30;
-        $houers = floor($res);
-        //Получаем количество пройденных минут.
-        $count = $res - floor($res);
-        $min = $count * 60;
-        if (($degrees % 30) === 0) {
-            $min = '00';
-        }
-        if ($houers == 1) {
-            $houerStr = 'час';
-        }
-        else if ($houers == 2 || $houers == 3 || $houers == 4) {
-            $houerStr = 'часа';
-        }
-        else {
-            $houerStr = 'часов';
-        }
-
-//    var_dump($result);
-//    var_dump($min);
-        $result = "$houers $houerStr : $min";
-        return $result;
+    $res = $degrees / 30;
+    $houers = floor($res);
+    //Получаем количество пройденных минут.
+    $count = $res - floor($res);
+    $min = $count * 60;
+    if (($degrees % 30) === 0) {
+        $min = '00';
     }
+    if ($houers == 1) {
+        $houerStr = 'час';
+    }
+    else if ($houers == 2 || $houers == 3 || $houers == 4) {
+        $houerStr = 'часа';
+    }
+    else {
+        $houerStr = 'часов';
+    }
+
+
+//    if ($min >= 2 && $min < 5 || $min > 21 && $min < 25 || $min > 31 && $min < 35 || $min > 41 && $min < 45 || $min > 51 && $min < 55) {
+//    if ($min >= 2 && $min < 5 || $min > 21 && $min < 25 || $min > 31 && $min < 35 || $min > 41 && $min < 45 || $min > 51 && $min < 55) {
+//        $minStr = 'минуты';
+//    }
+//    else if ($min > 4 && $min < 21 || $min > 24 && $min < 31 || $min > 34 && $min < 41 || $min > 44 && $min < 51 || $min > 54 && $min < 60) {
+//        $minStr = 'минут';
+//    }
+
+    $result = "$houers $houerStr : $min $minStr";
+    return $result;
 
 }
 
-echo houresCount(260);
+ houresCount(161);
+
+function declension ($number) {
+
+    $someStr = (string)$number;
+    $condition = $someStr[strlen($someStr) - 1];
+//    var_dump($hourStr);
+    if ($condition >= 2 && ) {
+
+    }
+}

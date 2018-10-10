03308a3767739321f6aa30127422567a0f05ba7d
Antony <miroliubov.a@yandex.ru>
2018-10-10 10:01:41 +0300

    need rewiev

--- array_learning/hightYear.php
+++ array_learning/hightYear.php
@@ -1,37 +1,38 @@
 <?php
-
 //Вам нужно разработать программу, которая проверяла бы введенное пользователем число (год, является ли она високосным).
 // Число может быть в диапазоне от 1 до 9999.
 
 function hightYear($number) {
-	$result = '';
-	if (is_string($number)) {
-		echo 'Передана строка.';
-	}
-	else if ($number >= 100) {
-		$yearArr = (string)$number;
-		// var_dump($yearArr);
-		$lastButOneEl = $yearArr[strlen($yearArr) - 2];
-		$lastEl = $yearArr[strlen($yearArr) - 1];
-		$twoLastElements = $lastButOneEl . $lastEl;
-		// var_dump($twoLastElements);
-		if (($twoLastElements % 4) === 0) {
-			$result = 'Год является високсным';
-		}
-		else {
-			$result = 'Год не високосный';	
-		}
-	}
-	else if ($number < 100 && $number > 0) {
-		if (($number % 4) === 0) {
-			$result = 'Год является високосным';
-		}
-		else {
-			$result = 'Год не високсный';
-		}
-	}
-	return $result;
+    $result = '';
+    if ($number < 100 && $number > 0) {
+        if (($number % 4) === 0) {
+            $result = 'Год является високосным.';
+        }
+        else {
+            $result = 'Год не високсный.';
+        }
+    }
+    else if ($number > 0) {
+        $yearArr = (string)$number;
+        //Находим последний и предпоследний элементы и создаем из них число, далее проверям делится ли оно без остатка на 4, это и будет наш високосный год.
+        $lastButOneEl = $yearArr[strlen($yearArr) - 2];
+        $lastEl = $yearArr[strlen($yearArr) - 1];
+        $twoLastElements = $lastButOneEl . $lastEl;
+        //Вычесляем високосный ли год.
+        if (($twoLastElements % 4) === 0) {
+            $result = 'Год является високсным.';
+        }
+        else {
+            $result = 'Год не високосный.';
+        }
+    }
+    //Кейс когда год от 1 до 100.
+    else if ($number === 0 || $number <= 0 || is_string($number)) {
+        $result = 'Введены некорректные данные.';
+    }
+
+    return $result;
 }
 
-echo hightYear(0);
+echo hightYear(2014);
 ?>

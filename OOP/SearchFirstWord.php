effc3fe85249b10f1ad815c2d5a7db5ac1104d88
Antony <miroliubov.a@yandex.ru>
2018-10-24 21:57:15 +0300

    wip2
    Сделал метод для преобразования строки в массив.

--- OOP/SearchFirstWord.php
+++ OOP/SearchFirstWord.php
@@ -6,29 +6,25 @@
  * Строка может начинаться с буквы или, к примеру, с пробела или точки
  * В слове может быть апостроф и он является частью слова
  * Весь текст может быть представлен только одним словом и все +
-
 */
-
 class SearchFirstWord
 {
     /**
      * Метод убирает не нужные знаки припенания.
-
      */
     private function removeSpecialChars($str)
     {
         return preg_replace("|[^\d\w ]+|i"," ",$str);
     }
-
     /**
      * Метод превращает строку в массив.
-
      */
-//    public function stringToArray($someString)
-//    {
-//        return explode(' ', $someString);
-//    }
 
+   public function stringToArray($someString)
+   {
+       return explode(' ', $someString);
+ 
+   }
 //    private function checkFirstElem($val)
 //    {
 //        $this->removeSpecialChars($val);
@@ -39,30 +35,23 @@ class SearchFirstWord
 //        }
 //
 //    }
-
     public function searchWord($someWord)
     {
         $result = '';
-//        $this->stringToArray($someWord);
-        $arr = explode(' ', $someWord);
-//        var_dump($someWord);
-        for($i = 0; $i < count($arr); $i++) {
-            if (strlen($arr[$i]) > 1) {
-//                var_dump($arr[$i]);
-                
-            }
-        }
+        $strArr = $this->stringToArray($someWord);
+       var_dump($strArr);
+        // for($i = 0; $i < count($arr); $i++) {
+        //     if (strlen($arr[$i]) > 1) {
+
+        //        }
+        // }
         if (is_string($someWord[0])) {
             $result = $someWord[0];
 //            var_dump($result);
         }
-
         return $this->removeSpecialChars($result);
     }
 }
-
 $someString = new SearchFirstWord;
-//echo $someString->checkValue('22');
-//echo $someString->removeSpaces('lox=pidor');
 
 echo $someString->searchWord('l lox, pidor');

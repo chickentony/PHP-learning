c353a93fa9c023bc41204e177d1ac4df066bd6cf
Antony <miroliubov.a@yandex.ru>
2018-10-29 19:04:43 +0300

    refactor and small fixes
    Добавил проверку на пустую строку, приведение к строке во всех методах, эксепшн на пустую строку.

--- OOP/SearchFirstWord.php
+++ OOP/SearchFirstWord.php
@@ -2,56 +2,70 @@
 /**
  * Дана строка и нужно найти ее первое слово.
  * При решении задачи обратите внимание на следующие моменты:
- * В строке могут встречатся точки и запятые +
+ * В строке могут встречатся точки и запятые
  * Строка может начинаться с буквы или, к примеру, с пробела или точки
  * В слове может быть апостроф и он является частью слова
- * Весь текст может быть представлен только одним словом и все +
+ * Весь текст может быть представлен только одним словом и все
  */
 class SearchFirstWord
 {
+
     /**
      * Метод убирает не нужные знаки припенания.
      */
-    private function removeSpecialChars($str)
+    private function removeSpecialChars(string $someString)
     {
-        return preg_replace("|[^\d\w ]+|i"," ",$str);
+        return preg_replace("|[^\d\w ]+|i"," ", $someString);
     }
+
     /**
      * Метод превращает строку в массив.
      */
-    private function stringToArray($someString)
+    private function stringToArray(string $someString)
     {
         return explode(' ', $someString);
     }
 
-    private function removeShortLetters($value)
+    /**
+     * Метод убирает символы короче 2 из начала строки.
+     */
+    private function removeShortLetters(string $someSting)
     {
-        return preg_replace('/\b(\S{1,2}|\S{})\b/', "",  $value);
+        return preg_replace('/\b(\S{1,2}|\S{})\b/', "",  $someSting);
     }
 
-    // private function checkFirstElem($val)
-    // {
-    //     // $result = '';
-    //     $stringWithoutSpecialChars = $this->removeSpecialChars($val);
-    //     return $this->removeShortLetters($stringWithoutSpecialChars);
-        
-    //     // var_dump($stringWithoutShortLetters);
-    // }
-
-    public function searchWord($someWord)
+    /**
+     * Основной метод, осуществляет поиск первого слова в строке.
+     */
+    public function searchWord(string $someWords)
     {
-        $result = $this->removeSpecialChars($someWord);
-        $result = $this->removeShortLetters($result);
-        $result = $this->stringToArray($result);
-        foreach ($result as $key => $value) {
-            if ($value !== '') {
-                $res = $value;
-                break;
+        if ($someWords !== '') {
+            $words = $this->removeSpecialChars($someWords);
+            $words = $this->removeShortLetters($words);
+            $words = $this->stringToArray($words);
+            foreach ($words as $key => $value) {
+                if ($value !== '') {
+                    $result = $value;
+                    break;
+                }
             }
+            return $result;
+        } else {
+            throw new Exception('Передана пустая строка.');
         }
 
-        return $res;
+        return false;
     }
 }
+
 $someString = new SearchFirstWord;
-echo $someString->searchWord('ny pizdec!');
+
+try {
+
+    echo $someString->searchWord('test');
+}
+
+catch (Exception $e) {
+
+    echo 'Message: ' . $e->getMessage();
+}

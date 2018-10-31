f2c360e16e08acfeff2586706980910c2e811aac
Antony <miroliubov.a@yandex.ru>
2018-10-31 17:58:35 +0300

    wip5
    Добавил метод который убирает добавки. Почистил код, привел его к более удобному и правильному виду.

--- OOP/FastFood.php
+++ OOP/FastFood.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+/**
 Некая сеть фаст-фудов предлагает несколько видов гамбургеров:
 	маленький (50 рублей, 20 калорий);
 	большой (100 рублей, 40 калорий).
@@ -8,80 +8,91 @@
 	сыром (+ 10 рублей, + 20 калорий);
 	салатом (+ 20 рублей, + 5 калорий);
 	картофелем (+ 15 рублей, + 10 калорий).
-Дополнительно, гамбургер можно посыпать приправой (+ 15 рублей, 0 калорий) и полить майонезом (+ 20 рублей, + 5 калорий). 
+Дополнительно, гамбургер можно посыпать приправой (+ 15 рублей, 0 калорий) и полить майонезом (+ 20 рублей, + 5 калорий).
 Напишите программу, рассчитывающую стоимость и калорийность гамбургера
 */
 class FastFood
 {
-	// const SMALL_SIZE = false;
-	// const BIG_SIZE = false;
-	// const STUFFING_CHEESE = false;
-	// const STUFFING_SALAD = false;
-	// const STUFFING_POTATO = false;
-	// const TOPPING_MAYO = false;
-	// const TOPPING_SPICE = false;
-	public $smallSize; 
-	public $bigSize;
-	public $toppingMayo; 
-	public $toppingSpice;
-	public $stuffingCheese;
-	public $stuffingSalad;
-	public $stuffingPotato;
+    // const SMALL_SIZE = false;
+    // const BIG_SIZE = false;
+    // const STUFFING_CHEESE = false;
+    // const STUFFING_SALAD = false;
+    // const STUFFING_POTATO = false;
+    // const TOPPING_MAYO = false;
+    // const TOPPING_SPICE = false;
+//    public $smallSize;
+//    public $bigSize;
+//    public $toppingMayo;
+//    public $toppingSpice;
+//    public $stuffingCheese;
+//    public $stuffingSalad;
+//    public $stuffingPotato;
 
-	public function __construct($size, $stuffing) 
-	{
-		if ($size === 'small') {
-			$this->smallSize = true;
-			// self::SMALL_SIZE = true;
-		} elseif ($size === 'big') {
-			$this->bigSize = true;
-		} elseif ($stuffing === 'cheese') {
-			$this->stuffingCheese = true;
-		} elseif ($stuffing === 'salad') {
-			$this->stuffingSalad = true;
-		} elseif ($stuffing === 'potato') {
-			$this->stuffingPotato = true;
-		} else {
-			throw new Exception('Один из параметров указан ен верно.');
-		}	
-	}
+    private $size;
+    private $stuffing;
+    private $topping;
 
-	// public function addTopping($topping)
-	// {
-	// 	$result;
-	// 	if ($topping === 'mayo') {
-	// 		$result = $this->toppingMayo = 20;
-	// 	}
-	// 	if ($topping === 'spice') {
-	// 		$result = $this->toppingSpice = 15;
-	// 	}
-	// 	// if (empty($this->toppingSpice) || empty($this->toppingMayo)) {
-	// 	// 	$result = 'Ингредиенты уже добавлены.';
-	// 	// }
-	// 	return $result;
-	// 	// $this->TOPPING_MAYO = $topping;
-	// 	// $this->TOPPING_SPICE = $topping;
-	// }
-	// public function removeTopping($someTopping) 
-	// {
-	// 	$result;
-	// 	if ($someTopping === 'mayo') {
-	// 		$result = $this->toppingMayo = null;
-	// 	}
-	// 	if ($someTopping === 'spice') {
-	// 		$result = $this->toppingSpice = null;
-	// 	}
-	// 	return $result;
-	// }
+    public function __construct($size, $stuffing)
+    {
+        if (is_string($size) && is_string($stuffing)) {
+            $this->_setSize($size);
+            $this->_setStuffing($stuffing);
+        } else {
+            echo 'Переданные параметры не являются строкой';
+        }
+    }
+
+    private function _setSize($size)
+    {
+        if ($size === 'small') {
+            $this->size = 'small';
+        }
+        if ($size === 'big') {
+            $this->size = 'big';
+        }
+    }
+
+    private function _setStuffing($stuffing)
+    {
+        if ($stuffing === 'cheese') {
+            $this->stuffing = 'cheese';
+        }
+        if ($stuffing === 'salad') {
+            $this->stuffing = 'salad';
+        }
+        if ($stuffing === 'potato') {
+            $this->stuffing = 'potato';
+        }
+    }
+
+    public function getHamburger()
+    {
+        return $this->size . $this->stuffing;
+    }
+
+    /**
+      * Дополнительная добавка, нужно реализовать кейс с одновременным добавлением.
+     */
+    public function _setTopping($topping)
+    {
+        if ($topping === 'mayo') {
+            $this->topping = 'mayo';
+        }
+        if ($topping === 'spice') {
+            $this->topping = 'spice';
+        }
+    }
+
+    public function removeTopping()
+    {
+        if ($this->topping !== null) {
+            unset ($this->topping);
+        }
+    }
 }
 
-try {
-	$hamburger = new FastFood(1, 2);
-} catch(Exception $e) {
-	echo 'Message:' . $e->getMessage();
-} 
-
-$hamburger = new FastFood('s', 'cheese');
+$hamburger = new FastFood('small', 'cheese');
+$hamburger->_setTopping('spice');
+$hamburger->removeTopping();
 var_dump($hamburger);
-// var_dump($hamburger->addTopping('mayo'));
-// var_dump($hamburger->addTopping('spice'));
+echo $hamburger->getHamburger();

5668103e664186a6028071e19711005b2d356309
Antony <miroliubov.a@yandex.ru>
2018-11-10 13:36:11 +0300

    clean and refactor
    Случайно занес не тот код, заменил на правильный + почистил и отрефакторил его.

--- OOP/SearchFirstWord.php
+++ OOP/SearchFirstWord.php
@@ -1,67 +1,53 @@
-class FastFood
+<?php
+/**
+ * Дана строка и нужно найти ее первое слово.
+ * При решении задачи обратите внимание на следующие моменты:
+ * В строке могут встречатся точки и запятые +
+ * Строка может начинаться с буквы или, к примеру, с пробела или точки
+ * В слове может быть апостроф и он является частью слова
+ * Весь текст может быть представлен только одним словом и все +
+ */
+class SearchFirstWord
 {
-    // const SMALL_SIZE = false;
-    // const BIG_SIZE = false;
-    // const STUFFING_CHEESE = false;
-    // const STUFFING_SALAD = false;
-    // const STUFFING_POTATO = false;
-    // const TOPPING_MAYO = false;
-    // const TOPPING_SPICE = false;
-    public $smallSize;
-    public $bigSize;
-    public $toppingMayo;
-    public $toppingSpice;
-    public $stuffingCheese;
-    public $stuffingSalad;
-    public $stuffingPotato;
-
-    public function __construct($size, $stuffing)
+    /**
+     * Метод убирает не нужные знаки припенания.
+     */
+    private function removeSpecialChars($str)
     {
-        if ($size === 'small') {
-            $this->smallSize = true;
-            // self::SMALL_SIZE = true;
-        }
-        if ($size === 'big') {
-            $this->bigSize = true;
-        }
-        if ($stuffing === 'cheese') {
-            $this->stuffingCheese = true;
-        }
-        if ($stuffing === 'salad') {
-            $this->stuffingSalad = true;
-        }
-        if ($stuffing === 'potato') {
-            $this->stuffingPotato = true;
-        }
-        if ($size === null || $stuffing === null || $size === '' || $stuffing === '') {
-            throw new Exception('Один или несколько параметров переданы не верно.');
-        }
+        return preg_replace("|[^\d\w ]+|i"," ",$str);
     }
 
-    public function addTopping($topping)
+    /**
+     * Метод превращает строку в массив.
+     */
+    private function stringToArray($str)
     {
-        if ($topping === 'mayo') {
-            $this->toppingMayo = true;
-        }
-        if ($topping === 'spice') {
-            $this->toppingSpice = true;
-        }
+        return explode(' ', $str);
     }
 
-//    public function checkException()
-//    {
-//        try {
-//            if (new FastFood(null, null));
-//        } catch ()
-//    }
+    private function removeShortLetters($str)
+    {
+        return preg_replace('/\b(\S{1,3}|\S{})\b/', "",  $str);
+    }
 
+    /**
+     * Метод убирает из строки слова меньше трех букв.
+     */
+    public function searchWord($letter)
+    {
+        $result = $this->removeSpecialChars($letter);
+        $result = $this->removeShortLetters($result);
+        $result = $this->stringToArray($result);
+        foreach ($result as $key => $value) {
+            if ($value !== '') {
+                $res = $value;
+                break;
+            }
+        }
+
+        return $res;
+    }
 }
-try {
-    $hamburger = new FastFood('small', 'potato');
-} catch(Exception $e) {
-    echo 'Message:' . $e->getMessage();
-}
-$hamburger->addTopping('spice');
-var_dump($hamburger);
-// var_dump($hamburger->addTopping('mayo'));
-// var_dump($hamburger->addTopping('spice'));
+
+$someString = new SearchFirstWord;
+echo $someString->searchWord('etot je psrosto ne veroyatno');

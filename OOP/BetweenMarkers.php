6d025b4d1ca66dcfd739b58a3478d00f924cfb9e
Antony <miroliubov.a@yandex.ru>
2018-12-02 14:56:49 +0300

    Update BetweenMarkers.php
    Сделан вывод текста между двумя маркерами, нужно добавить все ограничения из задачи.

--- OOP/BetweenMarkers.php
+++ OOP/BetweenMarkers.php
@@ -6,34 +6,31 @@
  * Если нет конечного маркера, то концом считать конец строки
  * Если нет ни конечного, ни начального маркеров, то просто вернуть всю строку
  * Если конечный маркер стоит перед начальным, то вернуть пустую строку
-
  */
-
 class BetweenMarkers
 {
     public function getBetweenMarkers($text, $firstMarker, $secondMarker)
     {
+        $res = [];
+        $result = [];
         if (!empty($firstMarker) && !empty($secondMarker)) {
             $firstPos = strpos($text, $firstMarker);
             $secondPos = strpos($text, $secondMarker);
-//            $strArr= str_split($string);
-
-            $res = substr($text, $firstPos);
-            $result = stristr($res, $secondMarker, true);
-            var_dump($result);
-//            $strArr = str_split($res);
-
-//            var_dump($res);
-
-//            for ($i = 0; $i < count($strArr); $i++) {
-////                var_dump($strArr[$i]);
-//                if ($strArr[$i] === $firstMarker || $strArr[$i] === $secondMarker) {
-//                    var_dump($i);
-//                }
-//            }
+            $textAfterFirstMarker = substr($text, $firstPos);
+            // var_dump($res);
+            $textBelowSecondMarker = stristr($textAfterFirstMarker, $secondMarker, true);
+            // var_dump($result);
+           $textArr = str_split($textBelowSecondMarker);
+           for ($i = 1; $i < count($textArr); $i++) {
+               // var_dump($textArr[$i]);
+                array_push($res, $textArr[$i]);
+                // var_dump($res);
+           }
+           $result = implode($res);
+           var_dump($result);
         }
     }
 }
-
 $value = new BetweenMarkers();
-$value->getBetweenMarkers('eto -sanya/ kim', '-', '/');
+$value->getBetweenMarkers('Ваш шедевр готов!
+Не следует, однако забывать, что консультация с широким активом требуют от нас анализа направлений прогрессивного развития. Повседневная практика показывает, что начало повседневной работы по формированию позиции обеспечивает широкому кругу (специалистов {участие в формировании системы обучения кадров, соответствует насущным потребностям. Таким образом новая модель организационной деятельности/ требуют от нас анализа систем массового участия. Не следует, однако забывать, что постоянное информационно-пропагандистское обеспечение нашей деятельности позволяет оценить значение направлений прогрессивного    развития.','{' ,'/');

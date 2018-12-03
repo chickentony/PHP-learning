c81a25839fc46ff64e74f22be9fbb18cd28cf224
Antony <miroliubov.a@yandex.ru>
2018-12-03 17:59:42 +0300

    Update BetweenMarkers.php
    Добавил проверку на пустой начальный маркер.

--- OOP/BetweenMarkers.php
+++ OOP/BetweenMarkers.php
@@ -9,6 +9,13 @@
  */
 class BetweenMarkers
 {
+    private function emptyFirstMarker($value, $secondValue)
+    {
+        $result = stristr($value, $secondValue, true);
+
+        return $result;
+    }
+
     public function getBetweenMarkers($text, $firstMarker, $secondMarker)
     {
         $res = [];
@@ -20,17 +27,25 @@ class BetweenMarkers
             // var_dump($res);
             $textBelowSecondMarker = stristr($textAfterFirstMarker, $secondMarker, true);
             // var_dump($result);
-           $textArr = str_split($textBelowSecondMarker);
-           for ($i = 1; $i < count($textArr); $i++) {
-               // var_dump($textArr[$i]);
+            $textArr = str_split($textBelowSecondMarker);
+            for ($i = 1; $i < count($textArr); $i++) {
+                // var_dump($textArr[$i]);
                 array_push($res, $textArr[$i]);
                 // var_dump($res);
-           }
-           $result = implode($res);
-           var_dump($result);
+            }
+            $result = implode($res);
+//            var_dump($result);
         }
+        if ($firstMarker === '') {
+           $result =  $this->emptyFirstMarker($text, $secondMarker);
+        }
+
+        return $result;
     }
 }
 $value = new BetweenMarkers();
-$value->getBetweenMarkers('Ваш шедевр готов!
-Не следует, однако забывать, что консультация с широким активом требуют от нас анализа направлений прогрессивного развития. Повседневная практика показывает, что начало повседневной работы по формированию позиции обеспечивает широкому кругу (специалистов {участие в формировании системы обучения кадров, соответствует насущным потребностям. Таким образом новая модель организационной деятельности/ требуют от нас анализа систем массового участия. Не следует, однако забывать, что постоянное информационно-пропагандистское обеспечение нашей деятельности позволяет оценить значение направлений прогрессивного    развития.','{' ,'/');
+echo $value->getBetweenMarkers('Ваш шедевр готов!
+Не следует, однако забывать, что консультация с широким активом требуют от нас анализа направлений прогрессивного развития. 
+Повседневная практика показывает, что начало повседневной работы по формированию позиции обеспечивает широкому кругу (специалистов {участие в формировании системы обучения кадров, соответствует насущным потребностям. 
+Таким образом новая модель организационной деятельности/ требуют от нас анализа систем массового участия. 
+Не следует, однако забывать, что постоянное информационно-пропагандистское обеспечение нашей деятельности позволяет оценить значение направлений прогрессивного    развития.', '' ,'/');

94e1730193d0e31bff7ff2335b527550f47d3644
Antony <miroliubov.a@yandex.ru>
2018-12-26 10:20:01 +0300

    Update MostFrequent.php

--- OOP/MostFrequent.php
+++ OOP/MostFrequent.php
@@ -7,23 +7,18 @@ class MostFrequent
 {
     public function getMostFrequent($array)
     {
-//        $element = $array[0];
         $arrCounted = array_count_values($array);
-//        var_dump($arrCounted);
-        //$count = 0;
+        $result = [];
         for ($i = 0; $i < count($array); $i++) {
             foreach ($arrCounted as $k => $v) {
-//                var_dump($v);
-                if ($v <= 1) {
-                    $result = $array[$i];
-                } else {
+                if ($array[$i] === $k && $v > 1) {
                     $result = $array[$i];
                 }
             }
         }
-        var_dump($result);
+        return $result;
     }
 }
 
 $value = new MostFrequent();
-$value->getMostFrequent(['a', 'a', 'bi']);
+echo $value->getMostFrequent(['a', 'a', 'bi', 'bi', 'bi', 'a', 'g']);
